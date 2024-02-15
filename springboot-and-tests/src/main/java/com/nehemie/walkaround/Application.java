5be91c821e1b4f29e525cf77d36b5852cc490e54
NehemieMbg <nehemie.mbg@gmail.com>
2024-02-15 21:10:50 +0100

    test

---
+++ springboot-and-tests/src/main/java/com/nehemie/walkaround/Application.java
@@ -0,0 +1,13 @@
+package com.nehemie.walkaround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
