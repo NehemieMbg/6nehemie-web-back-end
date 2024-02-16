ee0f19723dec9cc336577346ab664c5f3f129b69
NehemieMbg <nehemie.mbg@gmail.com>
2024-02-16 09:28:27 +0100

    Author Table

---
+++ jpa/src/main/java/org/nehemie/jpa/models/Author.java
@@ -0,0 +1,61 @@
+package org.nehemie.jpa.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Author {
+    
+    @Id
+    @GeneratedValue
+    /*    
+            (
+            strategy = GenerationType.TABLE,
+            generator = "author_id_gen"
+    )
+    @SequenceGenerator(
+            name = "author_sequence",
+            sequenceName = "author_sequence",
+            allocationSize = 1
+    )
+    @TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",Ê
+            allocationSize = 1
+    )*/
+    private Integer id;
+    
+    @Column(
+            name = "f_name",
+            length = 35
+    )
+    private String firstName;
+    
+    private String lastName;
+    
+    @Column(
+            unique = true,
+            nullable = false
+    )
+    private String email;
+    
+    private int age;
+    
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+    
+    @Column(
+            insertable = false // when first created
+    )
+    private LocalDateTime lastModified;
+}
